2654da6d8175a55a4b1fb3dc193dec70b0502611
VictorMMorant <victormmorant21@gmail.com>
2016-05-24 11:50:39 +0200

    Preprocesing updated

--- preprocessing.py
+++ preprocessing.py
@@ -65,7 +65,7 @@ def handleToc(slides):
 
 handleSlideshow(dom)
 """
-
+dictCategories = {'RESTAURANT#GENERAL':1,'RESTAURANT#PRICES':3,'RESTAURANT#QUALITY':5,'RESTAURANT#STYLE_OPTIONS':7,'RESTAURANT#MISCELLANEOUS':9,'FOOD#GENERAL':11,'FOOD#PRICES':13,'FOOD#QUALITY':15,'FOOD#STYLE_OPTIONS':17,'FOOD#MISCELLANEOUS':19,'DRINKS#GENERAL':21,'DRINKS#PRICES':23,'DRINKS#QUALITY':25,'DRINKS#STYLE_OPTIONS':27,'DRINKS#MISCELLANEOUS':29,'AMBIENCE#GENERAL':31,'AMBIENCE#PRICES':33,'AMBIENCE#QUALITY':35,'AMBIENCE#STYLE_OPTIONS':37,'AMBIENCE#MISCELLANEOUS':39,'SERVICE#GENERAL':41,'SERVICE#PRICES':43,'SERVICE#QUALITY':45,'SERVICE#STYLE_OPTIONS':47,'SERVICE#MISCELLANEOUS':49,'LOCATION#GENERAL':51,'LOCATION#PRICES':53,'LOCATION#QUALITY':55,'LOCATION#STYLE_OPTIONS':57,'LOCATION#MISCELLANEOUS':59}
 def getText(nodelist):
     rc = []
     for node in nodelist:
@@ -81,11 +81,9 @@ def load_data(path):
 
 def getXmlDom(path):
 	f = open(path,'r');
-	doc = "".join(f.readlines())
+	doc = ''.join(f.readlines())
 	dom = xml.dom.minidom.parseString(doc)
 	return dom
-
-
 
 def handleReviews(dom):
 	reviews = dom.getElementsByTagName("Review")
@@ -98,7 +96,7 @@ def handleReview(r):
 		handleSentence(s)
 
 def handleSentence(s):
-	text = handleText(s.getElementsByTagName("text")[0])
+	text = removePunctuation(handleText(s.getElementsByTagName("text")[0]))
 	opinions = handleOpinions(s.getElementsByTagName("Opinion"))
 	tagIOB(text,opinions)
 
@@ -116,7 +114,43 @@ def handleOpinions(opinions):
 	return result
 
 def tagIOB(text,opinions):
-	print(text,opinions)
+    print(text)
+    textList = [[word, 0] for word in text.split()]
+    
+    for o in opinions:
+        targetList = o[0].split()
+        if targetList[0] != 'NULL':
+            print(findFirstTarget(textList,targetList))
+        
+def findFirstTarget(textList,targetList):
+    i = 0
+    while(i < len(textList) and textList[i][0] != targetList[0]):
+        #print(textList[i][0],targetList[0])
+        i+=1
+    if i < len(textList):
+        return i;
+    else:
+        return -1
+def getIdCategory(category):
+    return dictCategories[category]
 
 if __name__ == '__main__':
 	load_data("./data/train.xml")
+
+
+
+############
+# CLEANING #
+############
+
+def remove_punct(text):
+  tokens = text.split();
+  punctuation = """, . 
+  ¡ ! 
+  ? ¿ = )
+   ( / & % $ · [ ] { } 
+    - _ * ^ : \" &lt; &gt; 
+    RT ... ' """
+  return [word for word in tokens if word not in punctuation.split()]
+
+
